b35c0e2de6a310f377b8ce90ece11eb5b8b5c460
Lee Beenen <leebeenen@gmail.com>
2022-02-14 13:24:59 +0100

    rename method

--- 2021/Day1.py
+++ 2021/Day1.py
@@ -4,29 +4,29 @@ arr = returnFileAsArrayOfInt(1, False)
 
 
 def p1():
-    increases = 0
+    inc = 0
     prev = arr[0]
 
     for i in range(0, len(arr)):
         if arr[i] > prev:
-            increases += 1
+            inc += 1
 
         prev = arr[i]
 
-    return increases
+    return inc
 
 
 def p2():
-    increases = 0
+    inc = 0
     prev = arr[0] + arr[1] + arr[2]
 
     for i in range(0, len(arr) - 2):
         sum = arr[i] + arr[i + 1] + arr[i + 2]
         if sum > prev:
-            increases += 1
+            inc += 1
 
         prev = sum
-    return increases
+    return inc
 
 
 if __name__ == "__main__":
