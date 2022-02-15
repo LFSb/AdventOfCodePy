290ce70f893074d833a0fdb13190e41bdcc03bf4
Lee Beenen <leebeenen@gmail.com>
2022-02-15 13:37:39 +0100

    Day3 p2, slight refactor

--- 2021/Day3.py
+++ 2021/Day3.py
@@ -1,6 +1,15 @@
 from Util import returnFileAsArray
 
-arr = returnFileAsArray(3, True)
+arr = returnFileAsArray(3, False)
+
+
+def returnSlicedPostionOfArray(index, arr):
+    ret = ""
+
+    for y in range(0, len(arr)):
+        ret += arr[y][index]
+
+    return ret
 
 
 def p1():
@@ -11,10 +20,8 @@ def p1():
     epsilon = ""
 
     for x in range(0, lineLength):  # depth
-        res = ""
 
-        for y in range(0, len(arr)):
-            res += arr[y][x]
+        res = returnSlicedPostionOfArray(x, arr)
 
         ones = res.count('1')
 
@@ -28,21 +35,38 @@ def p1():
     return int(gamma, 2) * int(epsilon, 2)
 
 
-def p2():
-    lineLength = len(arr[0])
-    halfway = len(arr) / 2
+def bitwiseFilterArray(arr, isMsb):
 
-    lsr = ""
-    ogr = ""
+    lineLength = len(arr[0])
+
+    result = ""
 
     for x in range(0, lineLength):  # depth
-        res = ""
+        res = returnSlicedPostionOfArray(x, arr)
 
-        for y in range(0, len(arr)):
-            res += arr[y][x]
+        halfway = len(arr) / 2
 
-        ones = res.count('1')
+        msb = '1' if res.count('1') >= halfway else '0'
 
+        #print(f"filtering msb {msb} on position {x}")
+
+        if(isMsb):
+            arr = list(filter(lambda l: l[x] == msb, arr))
+        else:
+            arr = list(filter(lambda l: l[x] != msb, arr))
+
+        if(len(arr) == 1):
+            result = arr[0]
+
+    return result
+
+
+def p2():
+
+    lsr = bitwiseFilterArray(arr, True)
+    ogr = bitwiseFilterArray(arr, False)
+
+    return int(lsr, 2) * int(ogr, 2)
 
 if __name__ == "__main__":
     print(p1())
